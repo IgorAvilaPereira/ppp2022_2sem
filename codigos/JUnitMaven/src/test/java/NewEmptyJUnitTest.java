df80150f33b92681ea96f87ec11df488b3d110aa
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2022-11-25 21:57:02 -0300

    junit

---
+++ codigos/JUnitMaven/src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author iapereira
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+//        System.out.println("oi");
+        assertEquals("oi", true, true);
+    }
+}
